3c005ff3d08cef9a0b88f3ff13ee700d54be3dbb
Eric Lindesay <psyel1@nottingham.ac.uk>
2023-06-14 12:35:44 +0100

    Portfolio: Use masonry to format the cards better

---
+++ app.js
@@ -0,0 +1,35 @@
+
+console.log("Test");
+
+// const grid = document.querySelector('.row');
+const grid = document.getElementById("masonryDiv");
+console.log(grid.classList);
+var msnry = new Masonry(grid);
+
+console.log("After msnry");
+
+function immediateReload() {
+    reloadMasonry();
+    // msnry.layout();
+}
+
+function delayedReload() {
+    reloadMasonry();
+}
+
+function reloadMasonry() {
+    console.log("RELOAD");
+    const collapseThings = document.getElementsByClassName("seeMoreCollapse");
+    for (i = 0; i < collapseThings.length; i++) {
+        doReload(collapseThings[i]);
+    }
+}
+
+function doReload(thing) {
+    $(thing).on('shown.bs.collapse', function () {
+        msnry.layout();
+    });
+    $(thing).on('hidden.bs.collapse', function () {
+        msnry.layout();
+    });
+}
